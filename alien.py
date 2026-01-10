d907c96fa97de3473b9d8801c8c8a7b4f24e1d51
David-Joslin <davidjoslin2022@gmail.com>
2026-01-10 10:21:50 +0000

    Add files via upload

--- alien.py
+++ alien.py
@@ -7,7 +7,7 @@ WIDTH = 500
 HEIGHT = 500
 TITLE = "Alien blaster"
 
-alien = Actor("alien.png")
+alien = Actor("alien.jpg")
 message = ""
 def draw():
     screen.clear()
@@ -28,6 +28,39 @@ def on_mouse_down(pos):
         place()
     else:
         message = "Try again!"
+
+alien2 = Actor("alien2.png")
+score = 0
+
+def place2():
+    alien2.x = random.randint(50, WIDTH - 50)
+    alien2.y = random.randint(50, HEIGHT - 50)
+
+place2()
+
+def on_mouse_down(pos):
+    global message, score
+    if alien.collidepoint(pos):
+        pygame.mixer.music.play()
+    
+        score += 1
+        message = "Good shot!"
+        place()
+    elif alien2.collidepoint(pos):
+        score -= 1
+        message = "Wrong alien!"
+        place2()
+    else:
+        message = "Try again!"
+
+def draw():
+    screen.clear()
+    screen.blit("nightsky.png", (0, 0))
+    alien.draw()
+    alien2.draw()
+    screen.draw.text(message, center=(250, 10), fontsize=30)
+    screen.draw.text("Score: " + str(score), topleft=(10, 10), fontsize=30)
+
 
 
 pgzrun.go()
