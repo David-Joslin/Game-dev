e557dd3ff84db169f6b0742e7abc6a1553b6932a
David-Joslin <davidjoslin2022@gmail.com>
2026-02-03 19:56:15 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -2,58 +2,26 @@ import pgzrun
 import random
 import time
 
-WIDTH = 600
-HEIGHT = 500
+WIDTH = 800
+HEIGHT = 600
 
-score = 0
-game_over = False
-bee = Actor("bee")
-bee.pos = (100,100)
-flower = Actor("flower")
-flower.pos = (200,200)
-message = " "
+TITLE = "Satellite game"
+satellites = []
+lines = []
+next_satellite = 0
+num_of_satellites = 10
+start_time = 0
+total_time = 0
+end_time = 0
+def create_satellite():
+    global start_time
+    for count in range(0,num_of_satellites):
+        satellite = Actor("satellite")
+        satellite.pos = (random.randint(30, WIDTH - 30),(random.randint(40, HEIGHT - 40)))
+        satellites.append(satellite)
+    start_time = time.time()#used to get current time of ur computer
+
 def draw():
-    screen.blit("bg",(0,0))
-    flower.draw()
-    bee.draw()
-    screen.draw.text("Score: " + str(score),color="black", topleft=(10,10))
-    if game_over:
-        screen.fill("pink")
-        global message
-        message = "Times up! \nYour Final Score:"
-        screen.draw.text(message + str(score),midtop = (WIDTH/2,20),
-                         fontsize = 50,color = "red")
-        
-def place_flower():
-    flower.x = random.randint(30,570)
-    flower.y = random.randint(40,460)
+    pass
 
-def time_up():
-    global game_over
-    game_over = True
-
-def update():
-    global score
-    if keyboard.left:
-        bee.x = bee.x - 2
-
-    if keyboard.right:
-        bee.x = bee.x + 2
-
-    if keyboard.up:
-        bee.y = bee.y - 2
-
-    if keyboard.down:
-        bee.y = bee.y + 2
-    flower_collected = bee.colliderect(flower)
-    if flower_collected == True:
-        score += 10
-        place_flower()
-clock.schedule(time_up, 60.0)
 pgzrun.go()
-    
-
-
-
-
-
