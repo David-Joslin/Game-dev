f6c9c15891df993849fe8f9f2f3fdbd6d908dd1f
David-Joslin <davidjoslin2022@gmail.com>
2026-02-17 19:58:06 +0000

    Add files via upload

---
+++ Galoga_game/main1.py
@@ -0,0 +1,22 @@
+import pgzrun
+import random
+
+WIDTH = 750
+HEIGHT = 600
+TITLE = "Galoga game"
+BLUE = (0,0,255)
+
+ship = Actor("galoga")
+bug = Actor("bug")
+
+ship.pos()
+
+
+
+
+
+
+
+
+
+pgzrun.go()
